a143502cee43b7cd65aa34a4092cbb4d273dad06
Yukeshhbp <121503970+Yukeshhbp@users.noreply.github.com>
2025-05-05 16:38:46 +0530

    Add files via upload

---
+++ Chatbot.py
@@ -0,0 +1,36 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage
+from dotenv import load_dotenv
+import os
+
+#load_dotenv()
+#api_key = os.getenv("GOOGLE_API_KEY")
+api_key = "AIzaSyCrW1ltMGD3cVuK_7P6ZI8fCWrrIf8vzz8"
+import streamlit as st
+
+def get_gemini_response(input_text):
+    llm = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    google_api_key=api_key,
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2
+)
+    message = [HumanMessage(content=input_text)]
+    response=llm.invoke(message)
+    return response.content
+
+
+st.set_page_config(page_title = "Q&A demo")
+st.header("Langchain Application")
+
+input = st.text_input("Input:",key="input")
+
+
+submit = st.button("Generate")
+
+if submit:
+    response = get_gemini_response(input)
+    st.subheader("The Response is ")
+    st.write(response)
