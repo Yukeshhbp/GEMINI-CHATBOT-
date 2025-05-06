44c5b2ccf37763229855794f4275cd30a848bfd2
Yukeshhbp <121503970+Yukeshhbp@users.noreply.github.com>
2025-05-06 10:15:03 +0530

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,35 +1,14 @@
-#import langchain_google_genai
-#from langchain_google_genai import ChatGoogleGenerativeAI
-#from langchain_core.messages import HumanMessage
+import langchain_google_genai
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 api_key = os.getenv("GOOGLE_API_KEY")
-#api_key = "GOOGLE_API_KEY"
 
-#import streamlit as st
-
-#def get_gemini_response(input_text):
-#    llm = ChatGoogleGenerativeAI(
-#        model="gemini-2.0-flash",
-#        google_api_key=api_key,
-#        temperature=0,
-#        max_tokens=None,
-#        timeout=None,
-#        max_retries=2
-#    )
-#    message = [HumanMessage(content=input_text)]
-#    response=llm.invoke(message)
-#    return response.content
 import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage
 
-# Direct API key (secure this in real apps)
-#api_key = "GOOGLE_API_KEY"
-
-# Gemini response function
 def get_gemini_response(input_text):
     llm = ChatGoogleGenerativeAI(
         model="gemini-2.0-flash",
